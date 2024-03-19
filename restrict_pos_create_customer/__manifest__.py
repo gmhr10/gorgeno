8b87406ab2e81f39b372ff470eca855d552ffa8f
gmhr10 <160429921+gmhr10@users.noreply.github.com>
2024-03-19 22:10:22 +0200

    Add files via upload

---
+++ restrict_pos_create_customer/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "restrict pos create customer",
+    "version": "1.0.0",
+    "sequence": -1000,
+    # "category": "hospital managment",
+    "author": "george gmhr",
+    "summary": "restrict pos create customer access right",
+    "depends": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/security.xml",
+    ],
+    "demo": [],
+    "application": True,
+    "installaple": True,
+    "auto install": False,
+    "license": "LGPL-3",
+}
